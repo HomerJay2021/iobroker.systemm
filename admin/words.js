65dcc4b79d54ab8848c8bb0ec71b8550f72be95b
HomerJay2021 <93794499+HomerJay2021@users.noreply.github.com>
2021-11-12 21:50:20 +0100

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -14,9 +14,9 @@ systemDictionary = {
         'pl': 'Ustawienia adaptera dla template',
         'zh-cn': 'template的适配器设置'
     },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
+    'hostName': {
+        'en': 'Hostname',
+        'de': 'Hostname',
         'ru': 'Опция 1',
         'pt': 'Opção 1',
         'nl': 'Optie 1',
@@ -25,17 +25,5 @@ systemDictionary = {
         'es': 'Opción 1',
         'pl': 'opcja 1',
         'zh-cn': '选项1'
-    },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
     }
 };
